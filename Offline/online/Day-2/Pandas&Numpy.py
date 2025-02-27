c846bda52a966eb04d7bacff76e1aa2fe5373ea5
EKLAVYA <bhandaribh12@gmail.com>
2025-02-27 10:29:01 +0530

    lateste changes

--- Offline/online/Day-2/Pandas&Numpy.py
+++
@@ -1,7 +0,0 @@
-'''pandas will not understand string as string but it will understand it as Obj.
-'''
-
-'''x=["india",1947,"delhi",200]
-data=[23,14,56,72,90]
-data1=[x for x in data if x%2==0]
-print(data1)'''
